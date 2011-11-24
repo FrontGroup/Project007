81d0a5c368895496c0c3f142069b5128f15dc99c
Petr Matějů <conpuerus@gmail.com>
2011-11-24 21:53:50 +0100

    Přidána role projektového manažera

---
+++ Spravce-client/src/client/PMRole.java
@@ -0,0 +1,42 @@
+package client;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+
+import javax.swing.JMenuItem;
+
+import client.AdminRole.CreateUserDialog;
+
+public class PMRole implements Role {
+	
+	private User user;
+
+	public PMRole(User user) {
+		super();
+		this.user = user;
+	}
+
+	@Override
+	public Iterable<JMenuItem> getMenuItems() {
+		JMenuItem show = new JMenuItem("Show teams");
+		show.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new FramePMShowTeams(user).setVisible(true);
+			}
+		});
+		JMenuItem create = new JMenuItem("Create new team");
+		create.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new FramePMEditTeam(user);
+
+			}
+		});
+		return Arrays.asList(show, create);
+	}
+
+}
