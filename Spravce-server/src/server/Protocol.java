b5c5108ef2aa02b8522d7e28ae9c897a8e6d13cf
bob-si <lucas@.(none)>
2011-11-24 17:31:01 +0100

    oprava_chyby_v_protocolu

--- Spravce-server/src/server/Protocol.java
+++ Spravce-server/src/server/Protocol.java
@@ -4,8 +4,7 @@ package server;
  * Trida, ktera zpracovava a ridi komunikaci s klienty.
  * @author lucas
  */
-public class Protocol
-{
+public class Protocol {
 
     private static Protocol instance = null;
     String db_address = "jdbc:mysql://beretis.sh.cvut.cz/oss?"
@@ -15,14 +14,11 @@ public class Protocol
     /**
      * Konstruktor
      */
-    private Protocol()
-    {
+    private Protocol() {
     }
 
-    public static Protocol getInstance()
-    {
-        if (instance == null)
-        {
+    public static Protocol getInstance() {
+        if (instance == null) {
             instance = new Protocol();
         }
         return instance;
@@ -33,61 +29,59 @@ public class Protocol
      * @param msg Prijata zprava.
      * @return Informace o zpracovani zpravy.
      */
-    public String process(String msg)
-    {
+    public String process(String msg) {
 
-        if (msg.startsWith("TEST"))
-        {
+        if (msg.startsWith("TEST")) {
             //uzivatel testuje pripojeni
             return "OK";
         }
-        if (msg.startsWith("LOGIN"))
-        {
+        if (msg.startsWith("LOGIN")) {
             //uzivatel se pripojuje (LOGIN ID PASS)
-            String id = msg.substring(2);
-            String pass = msg.substring(3);
+            String[] split = msg.split(" ");
+            String id = split[1];
+            String pass = split[2];
             String response = dbc.login(id, pass);
             return response;
         }
-        if (msg.startsWith("INFO"))
-        {
+        if (msg.startsWith("INFO")) {
             //zadost o atributy profilu uzivatele
-            String id = msg.substring(2);
+            String[] split = msg.split(" ");
+            String id = split[1];
             String response = dbc.getInfo(id);
             return response;
         }
-        if (msg.startsWith("ADD"))
-        {
+        if (msg.startsWith("ADD")) {
             //zadost o pridani uzivatele
-            String name = msg.substring(2);
-            String lastname = msg.substring(3);
-            int role = Integer.valueOf(msg.substring(4));
-            int group = Integer.valueOf(msg.substring(5));
-            String pass = msg.substring(6);
+            String[] split = msg.split(" ");
+            String name = split[1];
+            String lastname = split[2];
+            int role = Integer.valueOf(split[3]);
+            int group = Integer.valueOf(split[4]);
+            String pass = split[5];;
             String response = dbc.addUser(group, pass, name, lastname, role);
             return response;
         }
-        if (msg.startsWith("DEL"))
-        {
+        if (msg.startsWith("DEL")) {
             //zadost o smazani uzivatele
-            String id = msg.substring(2);
+            String[] split = msg.split(" ");
+            String id = split[1];
             String response = dbc.deleteUser(id);
             return response;
         }
-        if (msg.startsWith("CHANGE_PASS"))
-        {
+        if (msg.startsWith("CHANGE_PASS")) {
             //zadost o zmenu hesla uzivatele
-            String id = msg.substring(2);
-            String oldPass = msg.substring(3);
-            String newPass = msg.substring(4);
+            String[] split = msg.split(" ");
+            String id = split[1];
+            String oldPass = split[2];
+            String newPass = split[3];
             String response = dbc.changePass(id, oldPass, newPass);
             return response;
         }
-        if (msg.startsWith("ADMIN_CHANGE_PASS"))
-        {
+        if (msg.startsWith("ADMIN_CHANGE_PASS")) {
             //zadost o zmenu hesla uzivatele pres admina
-            String id = msg.substring(2);
-            String newPass = msg.substring(3);
+            String[] split = msg.split(" ");
+            String id = split[1];
+            String newPass = split[2];
             String response = dbc.adminChangePass(id, newPass);
             return response;
         }
